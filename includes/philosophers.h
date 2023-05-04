de0a1fcb908dc52d22e46a12004618806ff5cc41
tomatte <daniel.brandao2000@gmail.com>
2023-05-04 16:53:00 -0300

    crete t_philo struct

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:23:02 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/05/04 16:12:27 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:52:05 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,14 @@ typedef struct s_clst
 	struct s_clst	*next;
 	struct s_clst	*prev;
 }	t_clst;
+
+typedef struct s_philo
+{
+	int			left_fork;
+	int			right_fork;
+	int			num;
+	pthread_t	t;
+}	t_philo;
 
 void	validation(int argc, char *argv[]);
 
