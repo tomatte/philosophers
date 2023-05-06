69c60e4a00d43476e18053e72d49ee9585fe5196
tomatte <daniel.brandao2000@gmail.com>
2023-05-06 12:19:32 -0300

    philosophers think, eat and sleep, but don't die

--- src/routine.c
+++ src/routine.c
@@ -6,15 +6,11 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 08:53:36 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/05/06 12:10:01 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/05/06 12:18:47 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers.h>
-/*
-	before start eating (or any action) verify if philosopher is alive
-	restart timelapse when start eating
- */
 
 static void	philo_sleep(t_clst *node, t_philo *philo)
 {
@@ -23,6 +19,16 @@ static void	philo_sleep(t_clst *node, t_philo *philo)
 	ms = get_ms(philo);
 	print_msg(ms, philo->num, " is sleeping\n");
 	usleep(philo->data->sleep_ms * 1000);
+}
+
+static void	philo_think(t_clst *node)
+{
+	t_philo	*philo;
+	int		ms;
+
+	philo = node->content;
+	ms = get_ms(philo);
+	print_msg(ms, philo->num, " is thinking\n");
 }
 
 void	*routine(void *vnode)
@@ -39,6 +45,7 @@ void	*routine(void *vnode)
 	{
 		eat(node, philo);
 		philo_sleep(node, philo);
+		philo_think(node);
 	}
 	return (NULL);
 }
