78f53c5a3f32961fd1e926b386630a553e9e0c51
tomatte <daniel.brandao2000@gmail.com>
2023-05-20 12:23:55 -0300

    add support to one philo

--- philo_bonus/src/exec_routine.c
+++ philo_bonus/src/exec_routine.c
@@ -6,11 +6,23 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 10:15:34 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/05/20 11:55:20 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/05/20 12:23:20 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bonus.h>
+
+static void	take_fork(t_data *data)
+{
+	int		ms;
+
+	gettimeofday(&data->philo.start, NULL);
+	sem_wait(data->semaphore);
+	ms = get_ms(&data->philo);
+	print_msg(ms, data->philo.num, " has taken a fork\n", data);
+	while (1)
+		to_sleep(1000, data);
+}
 
 void	exec_routine(t_data *data)
 {
@@ -21,5 +33,8 @@ void	exec_routine(t_data *data)
 	ft_lstclear(&data->pid_list, free);
 	dead = 0;
 	data->dead = &dead;
-	routine(data);
+	if (data->philo_qty > 1)
+		routine(data);
+	else
+		take_fork(data);
 }
