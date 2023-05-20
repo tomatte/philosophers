3c70751643c64d584d40bdd416771caf8f0bd8a0
tomatte <daniel.brandao2000@gmail.com>
2023-05-20 10:40:11 -0300

    reduce leak

--- philo_bonus/src/exec_routine.c
+++ philo_bonus/src/exec_routine.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 10:15:34 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/05/16 17:31:53 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/05/19 01:31:06 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,6 @@ void	exec_routine(t_data *data)
 	pthread_mutex_init(&data->lastms_mutex, NULL);
 	pthread_create(&guardian, NULL, &start_guardian, (void *) data);
 	pthread_create(&t, NULL, &start_routine, (void *) data);
-	pthread_join(t, NULL);
 	pthread_join(guardian, NULL);
+	pthread_join(t, NULL);
 }
